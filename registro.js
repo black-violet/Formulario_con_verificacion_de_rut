cf455a034407a1167012243f22e11392393e08b4
black-violet <mariajose.vega.ma@gmail.com>
2020-03-13 16:16:58 -0300

    FC

---
+++ registro.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+    $('h1').click(function() {
+        $('#tabla').fadeIn(1000);
+    });
+});
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
+
+function checkRut(rut) {
+    // Despejar Puntos
+    var valor = rut.value.replace('.', '');
+    // Despejar Guión
+    valor = valor.replace('-', '');
+
+    // Aislar Cuerpo y Dígito Verificador
+    var cuerpo = valor.slice(0, -1);
+    dv = valor.slice(-1).toUpperCase();
+
+    // Formatear RUN
+    rut.value = cuerpo + '-' + dv
+
+    // Si no cumple con el mínimo ej. (n.nnn.nnn)
+    if (cuerpo.length < 7) { rut.setCustomValidity("RUT Incompleto"); return false; }
+
+    // Calcular Dígito Verificador
+    suma = 0;
+    multiplo = 2;
+
+    // Para cada dígito del Cuerpo
+    for (i = 1; i <= cuerpo.length; i++) {
+
+        // Obtener su Producto con el Múltiplo Correspondiente
+        index = multiplo * valor.charAt(cuerpo.length - i);
+
+        // Sumar al Contador General
+        suma = suma + index;
+
+        // Consolidar Múltiplo dentro del rango [2,7]
+        if (multiplo < 7) { multiplo = multiplo + 1; } else { multiplo = 2; }
+
+    }
+
+    // Calcular Dígito Verificador en base al Módulo 11
+    dvEsperado = 11 - (suma % 11);
+
+    // Casos Especiales (0 y K)
+    dv = (dv == 'K') ? 10 : dv;
+    dv = (dv == 0) ? 11 : dv;
+
+    // Validar que el Cuerpo coincide con su Dígito Verificador
+    if (dvEsperado != dv) { rut.setCustomValidity("RUT Inválido"); return false; }
+
+    // Si todo sale bien, eliminar errores (decretar que es válido)
+    rut.setCustomValidity('');
+}
